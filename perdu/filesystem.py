0abb8ad387c252c41d8c5898dda7b96f2d9e1782
Chris Mutel <cmutel@gmail.com>
2019-10-19 13:51:23 +0200

    Create parent directories as well

--- perdu/filesystem.py
+++ perdu/filesystem.py
@@ -2,4 +2,4 @@ import appdirs
 from pathlib import Path
 
 base_dir = Path(appdirs.user_data_dir("perdu-search", "perdu"))
-base_dir.mkdir(exist_ok=True)
+base_dir.mkdir(exist_ok=True, parents=True)
