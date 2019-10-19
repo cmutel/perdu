07ef98df0bdb238e2815de2ffca2ddfc5bede906
romainsacchi <r_s@me.com>
2019-10-19 22:32:36 +0200

    Added modal to make selection
    Added modal to make a match
    Suggested a couple of fixes to rpevent exceptions

---
+++ perdu/assets/js/matching.js
@@ -0,0 +1,144 @@
+// Populate last column of every row with a button
+
+var table = document.getElementById("search_results_table");
+// Iterate through rows
+for (var i = 0, row; row = table.rows[i]; i++) {
+    // Skip if parent of row is <thead>
+    if (!(row.parentElement.tagName == "THEAD")){
+        // Get first cell of the row
+        var first_cell = row.cells[0];
+        // Store its value
+        var search_item = first_cell.innerHTML;
+        // Get last cell of the row
+        var last_cell = row.cells[row.cells.length-1];
+        // Create button
+        var btn = document.createElement('input');
+        btn.type = "button";
+        btn.value = "Find a match";
+        btn.data = search_item
+        btn.addEventListener('click', function() {
+            show_modal(this);
+        }, false);
+        last_cell.appendChild(btn);
+    };
+
+}
+
+function query(val_to_query){
+
+    // Get the values of checked box
+    var array = []
+    var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+
+    var item_to_match = document.getElementById('label_item_to_match').innerHTML
+
+    for (var i = 0; i < checkboxes.length; i++) {
+      array.push(checkboxes[i].value)
+    }
+
+    if (array.length == 0) {
+        alert("At least one catalogue needs to be selected.")
+        return;
+    };
+
+    if (array.length>1){
+        var catalogue = "all";
+    } else {
+        var catalogue = array[0];
+    };
+
+    var opts = {
+      method: 'GET',
+      headers: {}
+    };
+    fetch('/get_search_results/'+catalogue+'/'+val_to_query, opts).then(function (response) {
+      return response.json();
+    })
+    .then(function (body) {
+        // Select table in modal
+        var table = document.getElementById("response_table")
+        table.innerHTML = "";
+        var th = document.createElement('tr');
+        th.innerHTML = '<th>Name</th><th>Action</th>'
+        table.appendChild(th);
+        for (var row in body){
+            if (body[row].length>0){
+                var tr = document.createElement('tr');
+                var btn = document.createElement('input');
+                btn.className = "button-primary";
+                btn.type = "submit";
+                btn.value = "Select";
+                btn.style = "float:right;margin:5px;";
+                btn.setAttribute('data', body[row][0].toString().slice(0,40));
+                btn.setAttribute('data-origin', item_to_match);
+                btn.onclick = function() {exit_modal(this);};
+
+                var div_label = document.createElement('div');
+                div_label.className="tooltip";
+                div_label.innerHTML = body[row][0].toString().slice(0,40);
+                var tooltip = document.createElement('span')
+                tooltip.className = "tooltiptext";
+                tooltip.innerHTML = body[row][1].toString();
+                div_label.append(tooltip);
+                var td_label = document.createElement('td');
+                var td_button = document.createElement('td');
+                td_label.append(div_label);
+                td_button.append(btn);
+                tr.append(td_label);
+                tr.append(td_button);
+                table.append(tr);
+            };
+        };
+
+    });
+};
+
+function show_modal(b){
+    // Select modal
+    var modal = document.getElementById('modal_match')
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    // Insert titles, etc.
+    var title_span = document.getElementById("item_to_match")
+    title_span.innerHTML = "<h2 id='label_item_to_match'>"+b.data+"</h2>";
+    // Fetch data
+    query(b.data);
+    // Display modal
+    modal.style.display = "block";
+    };
+
+function exit_modal(btn){
+    var match = btn.getAttribute("data");
+    var item_to_search_for = btn.getAttribute("data-origin");
+    var table = document.getElementById("search_results_table");
+
+    // Iterate through rows
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        if (row.cells[0].innerHTML == item_to_search_for){
+            row.cells[2].innerHTML = match;
+            // Hide modal
+            var modal = document.getElementById('modal_match')
+            modal.style.display = "none";
+            return;
+        };
+    };
+
+    console.log(btn)
+};
+
+// Populate table with search results as the search field is updated.
+var table = document.getElementById('search_input');
+table.addEventListener('input', function (evt) {
+    query(this.value);
+});
+
--- perdu/webapp.py
+++ perdu/webapp.py
@@ -17,13 +17,16 @@ from flask import (
     Response,
     send_file,
     url_for,
+    jsonify
 )
-from peewee import DoesNotExist
+from peewee import DoesNotExist, IntegrityError
 from werkzeug.utils import secure_filename
 
 perdu_app = Flask(
     "perdu_app", static_folder="perdu/assets/", template_folder="perdu/assets/templates"
 )
+
+import os
 
 UPLOAD_FOLDER = base_dir / "uploads"
 UPLOAD_FOLDER.mkdir(exist_ok=True)
@@ -32,6 +35,9 @@ ALLOWED_EXTENSIONS = {"xml", "spold", "csv"}
 # Default limit for file uploads is 5 MB
 perdu_app.config["MAX_CONTENT_LENGTH"] = 5 * 1024 * 1024
 perdu_app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# Generate a secret key for the session, otherwise flash() returns an exception
+perdu_app.config["SECRET_KEY"] = os.urandom(24)
 
 
 def allowed_file(filename):
@@ -105,19 +111,54 @@ def uploaded_file(hash):
 
 @perdu_app.route("/upload", methods=["POST"])
 def upload():
+
     # check if the post request has the file part
     if "file_upload" not in request.files:
         abort(400)
     file = request.files["file_upload"]
+
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == "":
         flash("No selected file")
         return redirect(url_for("index"))
     if file and allowed_file(file.filename):
+
         filename = secure_filename(file.filename)
-        file.save(str(UPLOAD_FOLDER / filename))
-        file_row = File.create(
-            name=filename, filepath=UPLOAD_FOLDER / filename, kind="csv"
-        )
+        # If the file has already been uploaded once, peewee will throw an IntegrityError exception because the
+        # entry is already in the DB
+        try:
+            file.save(str(UPLOAD_FOLDER / filename))
+            file_row = File.create(
+                name=filename, filepath=UPLOAD_FOLDER / filename, kind="csv"
+            )
+        except IntegrityError:
+            file_row = File.get(name=filename)
+            return redirect(url_for("uploaded_file", hash=file_row.sha256))
+
         return redirect(url_for("uploaded_file", hash=file_row.sha256))
+    else:
+        flash("The extension of the file provided may be wrong")
+        return redirect(url_for("index"))
+
+@perdu_app.route("/get_search_results/<catalogue>/<item_to_search_for>")
+def get_search_results(catalogue, item_to_search_for):
+
+    if catalogue == "gs1":
+        data = [[results[d] for d in results
+                 if d in ('brick', 'definition', 'family')]
+                for results in search_gs1(item_to_search_for)]
+    elif catalogue == "naics":
+        data = [[results[d] for d in results
+                 if d in ('brick', 'definition', 'family')]
+                for results in search_naics(item_to_search_for)]
+    else:
+        data = [[results[d] for d in results
+         if d in ('brick', 'definition', 'family')]
+        for results in search_gs1(item_to_search_for)]
+
+        data.extend([[results[d] for d in results
+                 if d in ('brick', 'definition', 'family')]
+                for results in search_naics(item_to_search_for)])
+
+    return jsonify(data)
